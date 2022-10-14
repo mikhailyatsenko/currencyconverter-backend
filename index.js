32a4c39ec627ea38a36ceb90e0097f421c1dbc73
mikhailyatsenko <misha.ggg@gmail.com>
2022-10-14 11:40:26 +0300

    fixed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const https = require("https");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
 const outputCurrency = "";
 
 app.get("/", (req, res) => {
