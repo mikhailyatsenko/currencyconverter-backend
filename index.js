ef50fd5aacfee8a6aff1671590016c53c8fcc364
mikhailyatsenko <misha.ggg@gmail.com>
2022-10-14 17:19:00 +0300

    fixed json

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const https = require("https");
 const app = express();
 const port = process.env.PORT || 3001;
+let inputCurrency;
 const outputCurrency = "";
 
 app.get("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Authorization");
-  res.end("server working");
-  const inputCurrency = req.query;
+  res.setHeader("Content-Type", "application/json");
+
+  inputCurrency = req.query;
   convert(inputCurrency, res);
 });
 
@@ -17,19 +17,25 @@ function convert(inputCurrency, response) {
   const url = `https://api.getgeoapi.com/v2/currency/convert?api_key=904feae0b722e7df9827cc79d154b91a6975cffc&from=${inputCurrency.from}&to=${inputCurrency.to}&amount=${inputCurrency.amount}&format=json`;
 
   https.get(url, (res) => {
-    let output = "";
+    let outputData = "";
+    let outputCurrency = {};
     if (res.statusCode !== 200) {
       console.log(`Something wrong: ${res}`);
       return;
     }
 
     res.on("data", (chunk) => {
-      output += chunk;
+      outputData += chunk;
     });
 
     res.on("end", () => {
-      console.log(JSON.stringify(output));
-      response.end(output);
+      outputData = JSON.parse(outputData);
+      outputCurrency.from = outputData.base_currency_name;
+      outputCurrency.to = outputData.rates[inputCurrency.to.toUpperCase()].currency_name;
+      outputCurrency.fromAmount = outputData.amount;
+      outputCurrency.toAmount = outputData.rates[inputCurrency.to.toUpperCase()].rate_for_amount;
+      console.log(outputCurrency);
+      response.end(JSON.stringify(outputCurrency));
     });
   });
 }
