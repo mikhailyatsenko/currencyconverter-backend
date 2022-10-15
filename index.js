e76728ff9e10db603915d9d5b1ab7e429e8a7233
mikhailyatsenko <misha.ggg@gmail.com>
2022-10-15 19:45:52 +0300

    1st working edition

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const https = require("https");
+const { resolve } = require("path");
 const app = express();
 const port = process.env.PORT || 3001;
-let inputCurrency;
-const outputCurrency = "";
 
 app.get("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Content-Type", "application/json");
-
-  inputCurrency = req.query;
+  const inputCurrency = req.query;
   convert(inputCurrency, res);
+});
+
+app.get("/rates", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Content-Type", "application/json");
+
+  const toCurrency = req.query.to;
+  getRates(toCurrency, res);
 });
 
 function convert(inputCurrency, response) {
@@ -34,10 +40,82 @@ function convert(inputCurrency, response) {
       outputCurrency.to = outputData.rates[inputCurrency.to.toUpperCase()].currency_name;
       outputCurrency.fromAmount = outputData.amount;
       outputCurrency.toAmount = outputData.rates[inputCurrency.to.toUpperCase()].rate_for_amount;
-      console.log(outputCurrency);
       response.end(JSON.stringify(outputCurrency));
     });
   });
+}
+
+function getRates(toCurrency, response) {
+  const urlEur = `https://api.getgeoapi.com/v2/currency/convert?api_key=904feae0b722e7df9827cc79d154b91a6975cffc&from=EUR&to=${toCurrency}&amount=1&format=json`;
+  const urlUsd = `https://api.getgeoapi.com/v2/currency/convert?api_key=904feae0b722e7df9827cc79d154b91a6975cffc&from=USD&to=${toCurrency}&amount=1&format=json`;
+  const urlGbp = `https://api.getgeoapi.com/v2/currency/convert?api_key=904feae0b722e7df9827cc79d154b91a6975cffc&from=GBP&to=${toCurrency}&amount=1&format=json`;
+  const outputRates = { toCurrency: toCurrency };
+
+  let myPromise = new Promise((resolve, reject) => {
+    https.get(urlEur, (res) => {
+      if (res.statusCode !== 200) {
+        console.log(`Something wrong: ${res}`);
+        return;
+      }
+
+      let outputData = "";
+
+      res.on("data", (chunk) => {
+        outputData += chunk;
+      });
+
+      res.on("end", () => {
+        outputData = JSON.parse(outputData);
+        outputRates["EUR"] = outputData.rates[toCurrency].rate_for_amount;
+        resolve();
+      });
+    });
+  });
+
+  myPromise
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        https.get(urlUsd, (res) => {
+          let outputData = "";
+
+          if (res.statusCode !== 200) {
+            console.log(`Something wrong: ${res}`);
+            return;
+          }
+
+          res.on("data", (chunk) => {
+            outputData += chunk;
+          });
+
+          res.on("end", () => {
+            outputData = JSON.parse(outputData);
+            outputRates["USD"] = outputData.rates[toCurrency].rate_for_amount;
+            resolve();
+          });
+        });
+      });
+    })
+    .then(() => {
+      https.get(urlGbp, (res) => {
+        let outputData = "";
+
+        if (res.statusCode !== 200) {
+          console.log(`Something wrong: ${res}`);
+          return;
+        }
+
+        res.on("data", (chunk) => {
+          outputData += chunk;
+        });
+
+        res.on("end", () => {
+          outputData = JSON.parse(outputData);
+          outputRates["GBP"] = outputData.rates[toCurrency].rate_for_amount;
+          console.log(outputRates);
+          response.end(JSON.stringify(outputRates));
+        });
+      });
+    });
 }
 
 app.listen(port, () => {
